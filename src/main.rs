ca2c96b7600078402d50cd7dbfb84f06c733395e
lewis <25719980+c4fe@users.noreply.github.com>
2020-06-08 16:48:45 +0100

    Parse PE file with goblin

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use std::env;
 use std::error;
+use std::io::Read;
+use std::fmt;
+use goblin::Object::PE;
 
 mod config;
 mod pe;
@@ -7,12 +10,33 @@ mod winapiwrapper;
 
 use config::Config;
 
+#[derive(Debug)]
+struct Error(String);
+
+impl fmt::Display for Error {
+	fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+		write!(f, "Image file parsing error: {}", self.0)
+	}
+}
+
+impl error::Error for Error {
+
+}
+
 fn main() -> Result<(), Box<dyn error::Error>> {
     let mut args = env::args();
     println!("args: {:?}", args);
 
-    let config = Config::from_args(&mut args);
-    println!("config: {:?}", config?);
+    let mut config = Config::from_args(&mut args)?;
+    println!("config: {:?}", config);
+
+	let mut file_bytes = Vec::new();
+	config.dll_file_mut().read_to_end(&mut file_bytes);
+
+	let pe = match goblin::Object::parse(file_bytes.as_slice())? {
+		PE(pe) => Ok(pe),
+		_ => Err(Box::new(Error("Expected PE file".to_string())))
+	}?;
 
     Ok(())
 }
