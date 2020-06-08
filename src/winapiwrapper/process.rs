a71915f117352fa8acb1538427b833fc9f4946bb
lewis <25719980+c4fe@users.noreply.github.com>
2020-06-08 17:12:40 +0100

    Process wrapper

---
+++ src/winapiwrapper/process.rs
@@ -0,0 +1,38 @@
+use std::ops::Drop;
+use winapi::um::processthreadsapi::OpenProcess;
+use winapi::um::handleapi::CloseHandle;
+use winapi::um::winnt::HANDLE;
+
+pub struct Process {
+	handle: HANDLE
+}
+
+impl Process {
+	pub fn from_pid(pid: u32, access: u32, inherit: bool) -> Self {
+		let handle = unsafe {
+			OpenProcess(access, inherit as i32, pid)
+		};
+
+		Self {
+			handle
+		}
+	}
+
+	pub fn close(&mut self) {
+		if self.handle.is_null() {
+			return;
+		}
+
+		unsafe {
+			CloseHandle(self.handle)
+		};
+
+		self.handle = 0 as HANDLE;
+	}
+}
+
+impl Drop for Process {
+	fn drop(&mut self) {
+		self.close();
+	}
+}
