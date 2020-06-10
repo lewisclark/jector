8b1a6b2338919e304cc7b1ba47440c334241c95c
lewis <25719980+c4fe@users.noreply.github.com>
2020-06-10 22:10:05 +0100

    Create PE buffer inside process

--- src/injector/manualmap.rs
+++ src/injector/manualmap.rs
@@ -3,6 +3,8 @@ use std::error;
 use super::injector::Injector;
 use crate::winapiwrapper::process::Process;
 use crate::winapiwrapper::virtualmem::VirtualMem;
+use crate::winapiwrapper::alloctype::AllocType;
+use crate::winapiwrapper::protectflag::ProtectFlag;
 
 pub struct ManualMapInjector {
 
@@ -10,7 +12,7 @@ pub struct ManualMapInjector {
 
 impl Injector for ManualMapInjector {
 	fn inject(process: Process, pe: PE) -> Result<(), Box<dyn error::Error>> {
-		VirtualMem::alloc(&process, 0, pe.size, 0, 0)?;
+		VirtualMem::alloc(&process, 0, pe.size, AllocType::MEM_COMMIT | AllocType::MEM_RESERVE, ProtectFlag::PAGE_EXECUTE_READWRITE)?;
 
 		Ok(())
 	}
