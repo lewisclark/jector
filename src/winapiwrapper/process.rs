bccef63eee1965d2a489f8c4f1cf50ab797f47bd
lewis <25719980+c4fe@users.noreply.github.com>
2020-06-10 18:35:54 +0100

    Error throwing in winapiwrapper::process

--- src/winapiwrapper/process.rs
+++ src/winapiwrapper/process.rs
@@ -1,4 +1,5 @@
 use std::ops::Drop;
+use super::error::Error;
 use winapi::um::handleapi::CloseHandle;
 use winapi::um::processthreadsapi::{OpenProcess, GetCurrentProcess};
 use winapi::um::winnt::HANDLE;
@@ -8,31 +9,47 @@ pub struct Process {
 }
 
 impl Process {
-    pub fn from_pid(pid: u32, access: u32, inherit: bool) -> Self {
+    pub fn from_pid(pid: u32, access: u32, inherit: bool) -> Result<Self, Error> {
         let handle = unsafe { OpenProcess(access, inherit as i32, pid) };
 
-        Self { handle }
+		if handle.is_null() {
+			Err(Error::new("OpenProcess returned NULL".to_string()))
+		}
+		else {
+			Ok(Self { handle })
+		}
     }
 
-	pub fn from_current() -> Self {
+	pub fn from_current() -> Result<Self, Error> {
 		let handle = unsafe { GetCurrentProcess() };
 
-		Self { handle }
+		if handle.is_null() {
+			Err(Error::new("OpenProcess returned NULL".to_string()))
+		}
+		else {
+			Ok(Self { handle })
+		}
 	}
 
-    pub fn close(&mut self) {
+    pub fn close(&mut self) -> Result<(), Error> {
         if self.handle.is_null() {
-            return;
+            return Err(Error::new("Null handle passed to Process::close".to_string()));
         }
 
-        unsafe { CloseHandle(self.handle) };
+        let ret = unsafe { CloseHandle(self.handle) };
 
-        self.handle = 0 as HANDLE;
+		if ret == 0 {
+			Err(Error::new("CloseHandle failed".to_string()))
+		}
+		else {
+			self.handle = 0 as HANDLE;
+			Ok(())
+		}
     }
 }
 
 impl Drop for Process {
     fn drop(&mut self) {
-        self.close();
+        self.close().unwrap();
     }
 }
