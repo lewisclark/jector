2f28bbc855bb5a6cb69e8cf4e066c2448ca55f9c
lewis <25719980+c4fe@users.noreply.github.com>
2020-06-10 21:47:44 +0100

    Make trait public

--- src/injector/injector.rs
+++ src/injector/injector.rs
@@ -2,7 +2,7 @@ use goblin::pe::PE;
 use super::error::Error;
 use crate::winapiwrapper::process::Process;
 
-trait Injector {
+pub trait Injector {
 	// TODO: Return base address of injected pe
 	fn inject(process: Process, pe: PE) -> Result<(), Error>;
 }
