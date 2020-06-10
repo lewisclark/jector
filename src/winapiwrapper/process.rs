4a0932777f4c8a189283e8d591935ef70fcae526
lewis <25719980+c4fe@users.noreply.github.com>
2020-06-10 20:11:48 +0100

    Implement wrappers around alloc and free

--- src/winapiwrapper/process.rs
+++ src/winapiwrapper/process.rs
@@ -1,9 +1,11 @@
 use std::ops::Drop;
-use std::ffi::c_void;
 use super::error::Error;
+use std::ffi::c_void;
 use winapi::um::handleapi::CloseHandle;
 use winapi::um::processthreadsapi::{OpenProcess, GetCurrentProcess};
 use winapi::um::winnt::HANDLE;
+use winapi::um::memoryapi::{VirtualAllocEx, VirtualFreeEx};
+use winapi::shared::minwindef::LPVOID;
 
 // Process
 
@@ -49,6 +51,15 @@ impl Process {
 			Ok(())
 		}
     }
+
+	pub fn handle(&self) -> Result<HANDLE, Error> {
+		if self.handle.is_null() {
+			Err(Error::new("Attempted to retrieve NULL handle".to_string()))
+		}
+		else {
+			Ok(self.handle)
+		}
+	}
 }
 
 impl Drop for Process {
@@ -65,14 +76,40 @@ pub struct VirtualMem<'a> {
 	size: usize
 }
 
-impl VirtualMem<'_> {
-        pub fn alloc(process: Process, address: *const c_void, size: usize, alloc_type: usize, protect: usize) {
+impl<'a> VirtualMem<'a> {
+        pub fn alloc(process: &'a Process, address: usize, size: usize, alloc_type: u32, protect: u32) -> Result<Self, Error> {
+			let mem = unsafe {
+				VirtualAllocEx(process.handle()?, address as LPVOID, size, alloc_type, protect)
+			};
 
-
+			if mem.is_null() {
+				Err(Error::new("VirtualAllocEx returned NULL".to_string()))
+			}
+			else {
+				Ok(Self {
+					process: process,
+					address: mem as *const c_void,
+					size: size
+				})
+			}
         }
 
-        pub fn free(&mut self) {
+        pub fn free(&mut self, freetype: u32) -> Result<(), Error> {
+			if self.address.is_null() {
+				return Err(Error::new("Tried to free null virtual memory region".to_string()));
+			}
 
+
+			let ret = unsafe {
+				VirtualFreeEx(self.process.handle()?, self.address as LPVOID, self.size, freetype)
+			};
+
+			if ret == 0 {
+				Err(Error::new("VirtualFreeEx failed".to_string()))
+			}
+			else {
+				Ok(())
+			}
         }
 }
 
