91fd07459b279d9d286b190fe1cf137486f1937a
lewis <25719980+c4fe@users.noreply.github.com>
2020-06-11 15:49:55 +0100

    Start on loader

--- src/injector/manualmap.rs
+++ src/injector/manualmap.rs
@@ -5,17 +5,17 @@ use crate::winapiwrapper::process::Process;
 use crate::winapiwrapper::processaccess::ProcessAccess;
 use crate::winapiwrapper::protectflag::ProtectFlag;
 use crate::winapiwrapper::virtualmem::VirtualMem;
+use bytebuffer::{ByteBuffer, Endian};
 use goblin::pe::PE;
-use bytebuffer::{Endian, ByteBuffer};
 use std::error;
 
 pub struct ManualMapInjector {}
 
 impl Injector for ManualMapInjector {
     fn inject(pid: u32, pe: PE, image: &Vec<u8>) -> Result<(), Box<dyn error::Error>> {
-		let hdr = pe.header;
-		let doshdr = hdr.dos_header;
-		let coffhdr = hdr.coff_header;
+        let hdr = pe.header;
+        let doshdr = hdr.dos_header;
+        let coffhdr = hdr.coff_header;
         let opthdr = match hdr.optional_header {
             Some(header) => Ok(header),
             None => Err(Box::new(Error::new("No optional header".to_string()))),
@@ -33,7 +33,7 @@ impl Injector for ManualMapInjector {
             false,
         )?;
 
-        let mut mem = VirtualMem::alloc(
+        let mut image_mem = VirtualMem::alloc(
             &process,
             0,
             pe_size,
@@ -41,26 +41,50 @@ impl Injector for ManualMapInjector {
             ProtectFlag::PAGE_EXECUTE_READWRITE,
         )?;
 
-        mem.set_free_on_drop(false);
+        image_mem.set_free_on_drop(false);
 
-        let mut buf = ByteBuffer::new();
-		buf.resize(pe_size);
-		buf.set_endian(Endian::LittleEndian);
+        let mut image_buf = ByteBuffer::new();
+        image_buf.resize(pe_size);
+        image_buf.set_endian(Endian::LittleEndian);
 
-		// Write headers
-		buf.write_bytes(&image[..opthdr.windows_fields.size_of_headers as usize]);
+        // Write headers
+        image_buf.write_bytes(&image[..opthdr.windows_fields.size_of_headers as usize]);
 
-		// Write sections
-		for section in pe.sections {
-			let start = section.pointer_to_raw_data as usize;
-			let size = section.size_of_raw_data as usize;
+        // Write sections
+        for section in pe.sections {
+            let start = section.pointer_to_raw_data as usize;
+            let size = section.size_of_raw_data as usize;
 
-			buf.set_wpos(section.virtual_address as usize);
-			buf.write_bytes(&image[start..start + size]);
-		}
+            image_buf.set_wpos(section.virtual_address as usize);
+            image_buf.write_bytes(&image[start..start + size]);
+        }
 
-        mem.write(buf.to_bytes().as_ptr(), buf.len())?;
+        // Write image buffer to image memory
+        image_mem.write(image_buf.to_bytes().as_ptr(), image_buf.len())?;
+
+        // Allocate loader memory
+        let mut loader_mem = VirtualMem::alloc(
+            &process,
+            0,
+            pe_size,
+            AllocType::MEM_COMMIT | AllocType::MEM_RESERVE,
+            ProtectFlag::PAGE_EXECUTE_READWRITE,
+        )?;
+
+        // Initialize loader buffer
+        let mut loader_buf = ByteBuffer::new();
+        loader_buf.resize(8092);
+        loader_buf.set_endian(Endian::LittleEndian);
+
+        // Write loader buffer to loader memory
+        loader_mem.write(loader_buf.to_bytes().as_ptr(), loader_buf.len())?;
 
         Ok(())
     }
 }
+
+// Loader
+
+struct LoaderInfo {}
+
+extern "system" fn loader() {}
