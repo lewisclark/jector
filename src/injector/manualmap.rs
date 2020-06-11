16da893361e821c34054202d5eefea6d7e21bc81
lewis <25719980+c4fe@users.noreply.github.com>
2020-06-11 12:01:08 +0100

    Allocate vec as buffer for writing to

--- src/injector/manualmap.rs
+++ src/injector/manualmap.rs
@@ -17,24 +17,38 @@ impl Injector for ManualMapInjector {
 			None => Err(Box::new(Error::new("No optional header".to_string())))
 		}?;
 
+		let pe_size = opthdr.windows_fields.size_of_image as usize;
+
         let process = Process::from_pid(
 			pid,
 			ProcessAccess::PROCESS_CREATE_THREAD
 				| ProcessAccess::PROCESS_QUERY_LIMITED_INFORMATION
-				| ProcessAccess::PROCESS_VM_OPERATION,
+				| ProcessAccess::PROCESS_VM_OPERATION
+				| ProcessAccess::PROCESS_VM_READ
+				| ProcessAccess::PROCESS_VM_WRITE,
             false,
         )?;
 
         let mut mem = VirtualMem::alloc(
             &process,
             0,
-            opthdr.windows_fields.size_of_image as usize,
+            pe_size,
             AllocType::MEM_COMMIT | AllocType::MEM_RESERVE,
             ProtectFlag::PAGE_EXECUTE_READWRITE,
         )?;
 
 		mem.set_free_on_drop(false);
 
+		let mut buf: Vec<u8> = Vec::with_capacity(pe_size);
+
+		write_dos_header();
+
+		mem.write(buf.as_ptr(), buf.len())?;
+
         Ok(())
     }
+}
+
+fn write_dos_header() {
+
 }
