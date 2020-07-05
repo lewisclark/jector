65f5fd1d2ff5d6c9d27c43b2e7d20a9a23fbb72c
lewis <me@lewis.to>
2020-07-05 22:14:39 +0100

    Resolve imports better

--- src/injector/manualmap.rs
+++ src/injector/manualmap.rs
@@ -9,6 +9,7 @@ use crate::winapiwrapper::remotethread::{self, RemoteThread};
 use crate::winapiwrapper::threadcreationflags::ThreadCreationFlags;
 use crate::winapiwrapper::virtualmem::VirtualMem;
 use bytebuffer::{ByteBuffer, Endian};
+use pelite::pe64::imports::Import::{ByName, ByOrdinal};
 use pelite::pe64::{Pe, PeFile};
 use std::error;
 use std::ffi::c_void;
@@ -23,6 +24,8 @@ use winapi::um::winnt::{
 };
 
 const NUM_BASE_RELOC_BLOCKS: usize = 2048;
+const NUM_IMPORT_DIRECTORIES: usize = 32;
+const NUM_IMPORT_PROC: usize = 128;
 
 type FnDllMain = unsafe extern "C" fn(HINSTANCE, DWORD, LPVOID) -> BOOL;
 type FnLoadLibraryA = unsafe extern "C" fn(LPCSTR) -> HMODULE;
@@ -116,6 +119,42 @@ impl Injector for ManualMapInjector {
             blocks
         };
 
+        // Collect imports
+        let import_directories = {
+            let mut import_directories = [ImportDirectory {
+                lib_name: 0,
+                names: [0; NUM_IMPORT_PROC],
+                vas: [0; NUM_IMPORT_PROC],
+            }; NUM_IMPORT_DIRECTORIES];
+
+            let mut i = 0;
+            for import in pe.imports()? {
+                let mut names = [0; NUM_IMPORT_PROC];
+                let mut vas = [0; NUM_IMPORT_PROC];
+
+                let mut j = 0;
+                for (va, proc) in Iterator::zip(import.iat()?, import.int()?) {
+                    names[j] = match proc? {
+                        ByName { hint, name } => image_mem.address() as usize + hint as usize + 2,
+                        ByOrdinal { ord } => ord as usize,
+                    };
+                    vas[j] = *va as usize;
+
+                    j += 1;
+                }
+
+                import_directories[i] = ImportDirectory {
+                    lib_name: image_mem.address() as usize + import.image().Name as usize,
+                    names: names,
+                    vas: vas,
+                };
+
+                i += 1;
+            }
+
+            import_directories
+        };
+
         // Construct LoaderInfo
         let lib_kernel32 = Library::load("kernel32.dll")?;
         let loader_info = LoaderInfo {
@@ -123,6 +162,7 @@ impl Injector for ManualMapInjector {
             image_delta: image_mem.address() as usize - pe.optional_header().ImageBase as usize,
             optional_header: pe.optional_header().clone(),
             base_reloc_blocks: base_reloc_blocks,
+            import_directories: import_directories,
             load_library: unsafe {
                 mem::transmute::<*const (), FnLoadLibraryA>(
                     lib_kernel32.proc_address("LoadLibraryA")?,
@@ -163,20 +203,6 @@ impl Injector for ManualMapInjector {
             )
         };
 
-        println!("pid -> {:x}", pid);
-        println!("image_mem -> {:x}", image_mem.address() as usize);
-        println!("loader_mem -> {:x}", loader_mem.address() as usize);
-        println!(
-            "entry point -> {:x}",
-            image_mem.address() as usize + pe.optional_header().AddressOfEntryPoint as usize
-        );
-        println!(
-            "entry point offset from base -> {:x}",
-            pe.optional_header().AddressOfEntryPoint
-        );
-        //std::thread::sleep_ms(60000);
-        println!("calling entrypoint...");
-
         // Spawn a thread to execute the loader buffer in the target process
         let thread = RemoteThread::new(
             &process,
@@ -190,8 +216,6 @@ impl Injector for ManualMapInjector {
 
         thread.wait(10000)?;
 
-        //loop {}
-
         Ok(())
     }
 }
@@ -203,6 +227,14 @@ struct BaseRelocBlock {
     typ: u8,
 }
 
+#[repr(C)]
+#[derive(Copy, Clone)]
+struct ImportDirectory {
+    lib_name: usize,
+    names: [usize; NUM_IMPORT_PROC], // Also contains ordinals
+    vas: [usize; NUM_IMPORT_PROC],
+}
+
 // Loader
 #[repr(C)]
 struct LoaderInfo {
@@ -210,6 +242,7 @@ struct LoaderInfo {
     image_delta: usize,
     optional_header: pelite::pe64::image::IMAGE_OPTIONAL_HEADER,
     base_reloc_blocks: [BaseRelocBlock; NUM_BASE_RELOC_BLOCKS],
+    import_directories: [ImportDirectory; NUM_IMPORT_DIRECTORIES],
     load_library: FnLoadLibraryA,
     get_proc_address: FnGetProcAddress,
 }
@@ -217,6 +250,7 @@ struct LoaderInfo {
 unsafe extern "C" fn loader(param: *mut winapic_void) -> u32 {
     let loader_info = mem::transmute::<*mut winapic_void, &LoaderInfo>(param);
 
+    // Base relocation
     {
         let mut i = 0;
         while i < NUM_BASE_RELOC_BLOCKS {
@@ -231,11 +265,50 @@ unsafe extern "C" fn loader(param: *mut winapic_void) -> u32 {
         }
     }
 
-    0
-    /*
-    if nt_header.OptionalHeader.AddressOfEntryPoint != 0 {
+    // Resolve imports
+    {
+        let mut i = 0;
+        while i < NUM_IMPORT_DIRECTORIES {
+            let import_directory = &loader_info.import_directories[i];
+
+            if import_directory.lib_name == 0 {
+                break;
+            }
+
+            let lib = (loader_info.load_library)(import_directory.lib_name as LPCSTR);
+
+            if lib as usize == 0 {
+                return 1;
+            }
+
+            let mut j = 0;
+            while j < NUM_IMPORT_PROC {
+                let va = import_directory.vas[j];
+
+                if va == 0 {
+                    break;
+                }
+
+                let name = import_directory.names[j];
+                let proc = (loader_info.get_proc_address)(lib, name as LPCSTR);
+
+                if proc as usize == 0 {
+                    return 1;
+                }
+
+                *(va as *mut usize) = proc as usize;
+
+                j += 1;
+            }
+
+            i += 1;
+        }
+    }
+
+    // Call entrypoint
+    if loader_info.optional_header.AddressOfEntryPoint != 0 {
         let entry = mem::transmute::<usize, FnDllMain>(
-            loader_info.image_base + nt_header.OptionalHeader.AddressOfEntryPoint as usize,
+            loader_info.image_base + loader_info.optional_header.AddressOfEntryPoint as usize,
         );
 
         entry(
@@ -246,7 +319,6 @@ unsafe extern "C" fn loader(param: *mut winapic_void) -> u32 {
     } else {
         1
     }
-    */
 }
 
 extern "C" fn loader_end() {}
