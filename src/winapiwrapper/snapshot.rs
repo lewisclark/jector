02310d8e92a619e66562295f4d53e7e084d376b1
lewis <me@lewis.to>
2020-07-14 21:25:10 +0100

    Implement iterator over snapshot thread entries

--- src/winapiwrapper/snapshot.rs
+++ src/winapiwrapper/snapshot.rs
@@ -1,7 +1,9 @@
 use super::error::Error;
 use super::snapshotflags::SnapshotFlags;
+use super::thread::Thread;
 use winapi::um::handleapi::INVALID_HANDLE_VALUE;
 use winapi::um::tlhelp32::CreateToolhelp32Snapshot;
+use winapi::um::tlhelp32::{Thread32First, Thread32Next, THREADENTRY32};
 use winapi::um::winnt::HANDLE;
 
 pub struct Snapshot {
@@ -22,6 +24,57 @@ impl Snapshot {
             Err(Error::new(
                 "CreateToolhelp32Snapshot returned an invalid handle".to_string(),
             ))
+        }
+    }
+
+    pub fn handle(&self) -> HANDLE {
+        self.handle
+    }
+
+    pub fn thread_entries(self) -> SnapshotThreadEntries {
+        SnapshotThreadEntries::new(self)
+    }
+}
+
+pub struct SnapshotThreadEntries {
+    snapshot: Snapshot,
+    is_first: bool,
+}
+
+impl SnapshotThreadEntries {
+    pub fn new(snapshot: Snapshot) -> Self {
+        Self {
+            snapshot,
+            is_first: true,
+        }
+    }
+}
+
+impl Iterator for SnapshotThreadEntries {
+    type Item = THREADENTRY32;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let mut thread_entry = THREADENTRY32 {
+            dwSize: std::mem::size_of::<THREADENTRY32>() as u32,
+            cntUsage: 0,
+            th32ThreadID: 0,
+            th32OwnerProcessID: 0,
+            tpBasePri: 0,
+            tpDeltaPri: 0,
+            dwFlags: 0,
+        };
+
+        let ret = if self.is_first {
+            self.is_first = false;
+
+            unsafe { Thread32First(self.snapshot.handle(), &mut thread_entry) }
+        } else {
+            unsafe { Thread32Next(self.snapshot.handle(), &mut thread_entry) }
+        };
+
+        match ret {
+            0 => None,
+            _ => Some(thread_entry),
         }
     }
 }
