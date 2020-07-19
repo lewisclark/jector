f5be86207e0be576d84690e5c659ea124d2c2afc
lewis <me@lewis.to>
2020-07-19 17:56:40 +0100

    Lay out foundations for external procaddress and library grabbing

--- src/winapiwrapper/library.rs
+++ src/winapiwrapper/library.rs
@@ -1,10 +1,12 @@
 use super::error::Error;
+use super::process::Process;
 use std::ffi::CString;
 use winapi::shared::minwindef::HMODULE;
 use winapi::um::libloaderapi::{GetProcAddress, LoadLibraryA};
 
 pub struct Library {
     handle: HMODULE,
+    is_external: bool,
 }
 
 impl Library {
@@ -23,11 +25,25 @@ impl Library {
         if handle.is_null() {
             Err(Error::new("LoadLibraryA returned NULL".to_string()))
         } else {
-            Ok(Self { handle })
+            Ok(Self {
+                handle,
+                is_external: false,
+            })
         }
     }
 
+    pub fn get_externally(process: &Process, name: &str) -> Result<Self, Error> {
+        Err(Error::new("Not implemented".to_string()))
+    }
+
     pub fn proc_address(&self, proc_name: &str) -> Result<*const (), Error> {
+        match self.is_external {
+            true => self.proc_address_external(proc_name),
+            false => self.proc_address_internal(proc_name),
+        }
+    }
+
+    fn proc_address_internal(&self, proc_name: &str) -> Result<*const (), Error> {
         let proc_name = match CString::new(proc_name) {
             Ok(cstr) => Ok(cstr),
             Err(e) => Err(Error::new(format!(
@@ -44,5 +60,9 @@ impl Library {
         } else {
             Ok(addr as *const ())
         }
+    }
+
+    fn proc_address_external(&self, proc_name: &str) -> Result<*const (), Error> {
+        Err(Error::new("Not implemented".to_string()))
     }
 }
