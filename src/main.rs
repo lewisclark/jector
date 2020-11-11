13355be1feb9c4c8d405318aff790bda5ba51555
lewis <me@lewis.to>
2020-11-11 15:35:04 +0000

    Improve arg handling with clap

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,50 @@
-use std::env;
+use clap::{App, Arg};
 use std::fs::File;
 use std::io::Read;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let args: Vec<String> = env::args().collect();
+    let matches = App::new("jector")
+        .version("1.0")
+        .author("Lewis Clark")
+        .arg(
+            Arg::with_name("pid")
+                .short("p")
+                .long("pid")
+                .value_name("PID")
+                .help("The PID of the process to inject into")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("file")
+                .short("f")
+                .long("file")
+                .value_name("DLL FILE")
+                .help("The DLL file to inject")
+                .takes_value(true)
+                .required(true),
+        )
+        .arg(
+            Arg::with_name("method")
+                .short("m")
+                .long("method")
+                .value_name("LOADLIBRARY/MANUALMAP")
+                .help("The injection method to use")
+                .takes_value(true)
+                .default_value("loadlibrary"),
+        )
+        .get_matches();
 
-    if args.len() != 4 {
-        let help = format!(
-            "{} <pid> <dll path> <method>\n\nAvailable methods:\n{}",
-            args[0], "manualmap\nloadlibrary",
-        );
+    let file_bytes = {
+        let mut buf = Vec::new();
+        File::open(matches.value_of("file").unwrap())?.read_to_end(&mut buf)?;
 
-        println!("{}", help);
-    } else {
-        let pid: u32 = args[1].parse()?;
-        let mut dll_bytes = Vec::new();
-        File::open(&args[2])?.read_to_end(&mut dll_bytes)?;
-        let method = args[3].parse()?;
+        buf
+    };
 
-        jector::inject_pid(pid, &dll_bytes, method)?;
+    let pid = matches.value_of("pid").expect("No PID given").parse()?;
+    let method = matches.value_of("method").unwrap().parse()?;
 
-        println!("Injected!");
-    }
+    jector::inject_pid(pid, &file_bytes, method)?;
 
     Ok(())
 }
